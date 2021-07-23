5591541cb64d502a67f3540499281bc14672a131
HibiAdelei <lilyadelame@gmail.com>
2021-07-22 17:40:38 -0700

    connection

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 require("console.table");
 
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    database: "employees_DB",
+    password: "password",
+});
+
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected`);
+    // init();
+});
+  
